2ad6939bb6f2ff9d7944a4683cb09471b852350c
PyInv🐍 <pyinv@192.168.1.8>
2025-03-08 21:25:37 +0000

    first commit

---
+++ src/main/java/com/docsmanage/docs/manage/DocsManageApplication.java
@@ -0,0 +1,13 @@
+package com.docsmanage.docs.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocsManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docsmanage/docs/manage/DocsManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docsmanage.docs.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
